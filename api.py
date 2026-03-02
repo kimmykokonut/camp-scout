9df5ca4fc20f0a9c7e482747db1eb7f5cb28a696
Kim Robinson <kimmykokonut@gmail.com>
2026-03-01 17:44:36 -0800

    refactor api call to accept user input month

--- api.py
+++ api.py
@@ -1,14 +1,16 @@
 import requests
+
 from config import API_BASE_URL, HEADERS
 
 # todo: add api call to search by name, grab ID (&other info?)
 
-def get_campground_availability(campground_id, start_date):
+def get_campground_availability(campground_id, month_input):
   """
   Fetch data from recreation.gov
   Returns: dict with campsites data or None if error
   """
-  url = f"{API_BASE_URL}/{campground_id}/month?start_date={start_date}T00%3A00%3A00.000Z"
+  month_str = str(month_input).zfill(2) # 4-> 04
+  url = f"{API_BASE_URL}/{campground_id}/month?start_date=2026-{month_str}-01T00%3A00%3A00.000Z"
 
   try:
     response = requests.get(url, headers=HEADERS)
