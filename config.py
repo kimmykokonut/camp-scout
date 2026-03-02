75bd0ecc0ad347a9f0a9e00de85233ab0838eb0e
Kim Robinson <kimmykokonut@gmail.com>
2026-03-01 17:55:28 -0800

    refactor main.py to take user input for month, cli successful return

--- config.py
+++ config.py
@@ -34,3 +34,5 @@ LOCATIONS = [
     "date": "2026-04-01"
   },
 ]
+
+RESERVE_BASE_URL = "https://www.recreation.gov/camping/campgrounds"
--- main.py
+++ main.py
@@ -1,25 +1,69 @@
 # entry point for chronjob
-from config import LOCATIONS
+import calendar
+
 from api import get_campground_availability
-from availability import check_availability
+from config import LOCATIONS
+from processor import extract_available_data, format_availability_display
 
 
 def main():
-    for location in LOCATIONS:
-        print(f"\nChecking: {location['name']}")
+    # Step 1: Hardcoded campground for now
+    campground_id = LOCATIONS[0]["campground_id"]
+    campground_name = LOCATIONS[0]["name"]
 
-        data = get_campground_availability(location["campground_id"], location["date"])
+    # step 2: get date from user
+    # todo: make this dynamic
+    print("\n=== Rec.gove Availability Checker 2026 ===\n")
+    print(f"Searching for: {campground_name}")
+    # NOTE: maybe a date picker?
+    month_input = input("Enter month to search for, eg. 4 for April: ")
+    # validate month
+    try:
+        month = int(month_input)
+        if month < 1 or month > 12:
+            print("Error: Month must be between 1-12")
+            return
+    except ValueError:
+        print("Error: Please enter a number between 1 and 12")
+        return
+    # get month name
+    month_name = calendar.month_name[month]
 
-        if data:
-            avail = check_availability(data)
-            if avail:
-                print(f"Available! {location['name']}")
-                print(avail)
-                # TODO: send notificaiton here
-            else:
-                print(f"Not available")
-        else:
-            print(f"Failed to fetch data")
+    print(f"\n Checking availabilty for {campground_name} for { month_name}...\n")
+
+    # Step 3: get data from api
+    data = get_campground_availability(campground_id, month_input)
+
+    if not data:
+        print("Failed to fetch data")
+        return
+
+    # Step 4: Process data
+    campsites_info = extract_available_data(data)
+
+    if campsites_info:
+        # Step 5: format and display
+        message = format_availability_display(campsites_info, campground_id, month_name)
+        print(message)
+        # Step 6: TBD notificaiotns
+    else:
+        print(f"No available sites found for {campground_name} in {month_name}")
+
+    # for location in LOCATIONS:
+    #     print(f"\nChecking: {location['name']}")
+
+    #     data = get_campground_availability(location["campground_id"], location["date"])
+
+    #     if data:
+    #         avail = extract_available_data(data)
+    #         if avail:
+    #             print(f"Available! {location['name']}")
+    #             print(avail)
+    #             # TODO: send notificaiton here
+    #         else:
+    #             print(f"Not available")
+    #     else:
+    #         print(f"Failed to fetch data")
 
 
 if __name__ == "__main__":
